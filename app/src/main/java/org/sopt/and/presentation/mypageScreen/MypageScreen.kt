32230ceff32874a5f0382a17da50b70981aad3ed
tunaunnie <ygw021227@gmail.com>
2024-12-04 02:25:17 +0900

    initial commit

--- app/src/main/java/org/sopt/and/presentation/mypageScreen/MypageScreen.kt
+++ app/src/main/java/org/sopt/and/presentation/mypageScreen/MypageScreen.kt
@@ -13,7 +13,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -24,9 +23,6 @@ import org.sopt.and.ui.components.MypageScreen.MyPageProfileSection
 import org.sopt.and.ui.components.MypageScreen.MyPageProfileSection2
 import org.sopt.and.ui.components.MypageScreen.MyPageSubSection
 import org.sopt.and.ui.theme.ANDANDROIDTheme
-import androidx.compose.runtime.livedata.observeAsState
-import org.sopt.and.presentation.main.UserViewModel
-
 
 @Composable
 fun MypageScreen(
